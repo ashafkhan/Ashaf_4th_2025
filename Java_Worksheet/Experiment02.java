156343e983a0c5968255f33ed443abdb23d6e5fe
Ashaf Khan <khanashaf02@gmail.com>
2025-01-11 10:11:16 +0530

    added Experiment02

---
+++ Java_Worksheet/Experiment02.java
@@ -0,0 +1,32 @@
+// Write a program that takes two strings representing numbers, converts them to wrapper classes, and performs basic arithmetic operations (addition, subtraction, multiplication, division).
+
+import java.util.Scanner;
+public class Experiment02 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        String num1=sc.nextLine();
+        System.out.println("Enter second number: ");
+        String num2=sc.nextLine();
+        
+        Double number1=Double.valueOf(num1);
+        Double number2=Double.valueOf(num2);
+
+        double add=number1 + number2;
+        double sub=number1 - number2;
+        double mult=number1 * number2;
+        double div=(number2 != 0) ? (number1/number2):null;
+
+        System.out.println("Results:");
+        System.out.println("Addition: " + add);
+        System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mult);
+        if (number2 != 0) {
+            System.out.println("Division: " + div);
+        } else {
+            System.out.println("Division: Cannot divide by zero.");
+        }
+    }
+}
+
+
