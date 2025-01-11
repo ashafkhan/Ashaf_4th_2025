da5295e43b25f25a419e2723b659ea7b34b0ddaa
Ashaf Khan <khanashaf02@gmail.com>
2025-01-11 10:08:08 +0530

    added aim as comments in Exp-01

--- Java_Worksheet/Experiment01.java
+++ Java_Worksheet/Experiment01.java
@@ -1,3 +1,5 @@
+//Create a class Animal with a method makeSound() that outputs a generic sound.The class should further contain derived classes Dog and Cat that override the makeSound() method to output specific sounds for each animal. Demonstrate polymorphism by creating an Animal reference that can hold objects of both Dog and Cat, and call the overridden makeSound() method at runtime.
+
 public class Experiment01 {
     public static void main(String[] args) {
         Animal animal1 = new Dog();
